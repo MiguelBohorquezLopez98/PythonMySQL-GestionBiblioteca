f3200343e22e83e065cf718478b1dcd92b8395a7
Miguel Bohórquez <dlsg1009@gmail.com>
2023-08-30 22:05:11 -0500

    Agregando archivo main

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ def iniciar():
             ver_libros()
         elif opcion == '3':
             buscar_libro()
+        elif opcion == '4':
+            modificar_libro()
+        elif opcion == '5':
+            eliminar_libro()
         elif opcion == '6':
             break
         else:
@@ -45,5 +49,25 @@ def buscar_libro():
     headers = ['ID', 'TITULO', 'AUTOR', 'ESTADO']
     tabla = tabulate(datos, headers, tablefmt='fancy_grid')
     print(tabla)
+
+def modificar_libro():
+    id = input('Ingrese el id del libro a modificar: ')
+    nuevo_valor = ''
+    respuesta = ''
+    campo = input('Seleccione el campo que desea modificar\n1. Titulo\n2. Autor\n3. Estado\n')
+    if campo == '1':
+        nuevo_valor = input('Ingrese el nuevo titulo del libro: ')
+    elif campo == '2':
+        nuevo_valor = input('Ingrese el nuevo autor del libro: ')
+    elif campo == '3':
+        nuevo_valor = input('Ingrese el nuevo estado del libro: ')
+    respuesta = modificar(id, campo, nuevo_valor)
+    print(respuesta)
+
+def eliminar_libro():
+    id = input('Ingrese el id del libro a eliminar: ')
+    respuesta = eliminar(id)
+    print(respuesta)
+
 
 iniciar()
