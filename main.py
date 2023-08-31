787ff295d6e87ef02b5ef36a11875f20a8e9c3c4
Miguel Bohórquez <dlsg1009@gmail.com>
2023-08-30 21:36:31 -0500

    Agregando archivo main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from tabulate import tabulate
 from conexion import *
+from libro import *
 con = conectar()
 
 def iniciar():
@@ -14,5 +15,35 @@ def iniciar():
         print('\t5. Eliminar un libro')
         print('\t6. Salir de la aplicación')
         opcion = input('Escoja una opción: ')
+        if opcion == '1':
+            nuevo_libro()
+        elif opcion == '2':
+            ver_libros()
+        elif opcion == '3':
+            buscar_libro()
+        elif opcion == '6':
+            break
+        else:
+            print('Escoja una opción correcta')
+
+def nuevo_libro():
+    titulo = input('Ingrese el título del libro: ')
+    autor = input('Ingrese el nombre del autor: ')
+    estado = 'Disponible'
+    respuesta = registrar(titulo, autor, estado)
+    print(respuesta)
+
+def ver_libros():
+    datos = mostrar()
+    headers = ['ID', 'TITULO', 'AUTOR', 'ESTADO']
+    tabla = tabulate(datos, headers, tablefmt='fancy_grid')
+    print(tabla)
+
+def buscar_libro():
+    id = input('Ingrese el id del libro: ')
+    datos = buscar(id)
+    headers = ['ID', 'TITULO', 'AUTOR', 'ESTADO']
+    tabla = tabulate(datos, headers, tablefmt='fancy_grid')
+    print(tabla)
 
 iniciar()
